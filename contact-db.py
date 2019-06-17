e6aefa92fd8032acb69b41e721a93a4beeb9202d
Prangon Ghose <pghose99@gmail.com>
2019-06-17 18:26:03 -0400

    basic functionality with menu works. needs further testing to finalize functionality. possible add reading from text file

--- contact-db.py
+++ contact-db.py
@@ -63,28 +63,32 @@ def main():
                     FROM contacts ORDER BY name'''
 
                     for row in cur.execute(sqlstr):
-                        print(str(row[0], row[1], row[2], row[3]))
+                        print(str(row[0]) + ' ' + str(row[1]) + ' ' + 
+                        str(row[2]) + ' ' + str(row[3]))
                 # order by email
                 elif user_input == '2':
                     sqlstr = '''SELECT name, email, phone, last_update
                     FROM contacts ORDER BY email'''
 
                     for row in cur.execute(sqlstr):
-                        print(str(row[0], row[1], row[2], row[3]))
+                        print(str(row[0]) + ' ' + str(row[1]) + ' ' + 
+                        str(row[2]) + ' ' + str(row[3]))
                 # order by phone number
                 elif user_input == '3':
                     sqlstr = '''SELECT name, email, phone, last_update
                     FROM contacts ORDER BY phone'''
 
                     for row in cur.execute(sqlstr):
-                        print(str(row[0], row[1], row[2], row[3]))
+                        print(str(row[0]) + ' ' + str(row[1]) + ' ' + 
+                        str(row[2]) + ' ' + str(row[3]))
                 # order by last updated
                 elif user_input == '4':
                     sqlstr = '''SELECT name, email, phone, last_update
                     FROM contacts ORDER BY last_update'''
 
                     for row in cur.execute(sqlstr):
-                        print(str(row[0], row[1], row[2], row[3]))
+                        print(str(row[0]) + ' ' + str(row[1]) + ' ' + 
+                        str(row[2]) + ' ' + str(row[3]))
                 # quit
                 elif user_input == '5':
                     print('\nExiting the sub-menu...\n')
@@ -106,20 +110,19 @@ def main():
             # if there is no existing contact with the same name, add contact
             if row is None:
                 cur.execute('''INSERT INTO contacts (name, email, phone, 
-                last_update) VALUES(%s, %s, %d, %s)''', 
+                last_update) VALUES(?, ?, ?, ?)''', 
                 (name, email, phone, str(datetime.datetime.now())))
 
-                print('\n%s added to the contacts database', name)
+                print('\n' + name + ' added to the contacts database')
             # if there is an existing contact, ask the user to continue
             else:
-                print('\nThere already exists a %s in the database.\n')
+                print('\n There already exists a ' + name + ' in the database.\n')
                 
                 # print contacts with the given name
-                sqlstr = '''SELECT name, email, phone, last_update FROM contacts
-                name = ''' + name
-
-                for row in cur.execute(sqlstr):
-                    print(str(row[0], row[1], row[2], row[3]))
+                for row in cur.execute('''SELECT name, email, phone, last_update FROM contacts
+                WHERE name = ?''', (name, )):
+                    print(str(row[0]) + ' ' + str(row[1]) + ' ' + 
+                        str(row[2]) + ' ' + str(row[3]))
 
                 string = '\n Continue to add ' + name + ' ? (Y/N)\n'
                 user_input = str(input(string)).upper()
@@ -128,12 +131,13 @@ def main():
                 # otherwise, don't add the contact
                 if user_input == 'Y':
                     cur.execute('''INSERT INTO contacts (name, email, phone, 
-                    last_update) VALUES(%s, %s, %d, %s)''', 
+                    last_update) VALUES(?, ?, ?, ?)''', 
                     (name, email, phone, str(datetime.datetime.now())))
 
-                    print('\n%s added to the contacts database', name)
+                    print('\n' + name + ' added to the contacts database.\n')
                 else:
-                    print('\nContinuing the program...\n')                
+                    print('\nContinuing the program...\n')              
+            conn.commit()            
 
         # delete a contact
         elif user_input == 'C':
@@ -141,7 +145,7 @@ def main():
 
             # check if there is an existing contact with the same name
             cur.execute('''SELECT name FROM contacts WHERE name = ?''',
-            (email,))
+            (name,))
             row = cur.fetchone()
 
             # if there is no contact with the given name, do nothing
@@ -151,18 +155,17 @@ def main():
                     given name. Continuing with the program...\n''')
             else:
                 cur.execute('''DELETE FROM contacts WHERE name = ?''', (name,))
-                print('''\nDeleted all contacts with the given name.\n''')
-                
+                print('''\nDeleted all contacts with the given name ''' + name + '''.\n''')
+            conn.commit()    
         # update a contact
         elif user_input == 'D':
             name = str(input('Contact Name: '))
 
             # print contacts with the given name
-            sqlstr = '''SELECT name, email, phone, last_update FROM contacts
-            name = ''' + name
-
-            for row in cur.execute(sqlstr):
-                print(str(row[0], row[1], row[2], row[3]))
+            for row in cur.execute('''SELECT name, email, phone, last_update FROM contacts
+            WHERE name = ?''', (name,)):
+                print(str(row[0]) + ' ' + str(row[1]) + ' ' + 
+                        str(row[2]) + ' ' + str(row[3]))
 
             while True:
                 # secondary menu for updating a contact
@@ -183,24 +186,24 @@ def main():
                     email = str(input('New Contact Email: '))
                     cur.execute('''UPDATE contacts SET email = ? WHERE name = ?''', (email, name))
                     
-                    print('\nEmail of %s updated.\n', name)
+                    print('\nEmail of ' + name + ' updated.\n')
                 # update phone number
                 elif user_input == '2':
                     phone = int(input('New Contact Phone: '))
                     cur.execute('''UPDATE contacts SET phone = ? WHERE name = ?''', (phone, name))
 
-                    print('\nPhone number of %s updated.\n', name)
+                    print('\nPhone number of ' + name + ' updated.\n')
                 # quit
                 elif user_input == '3':
                     print('\nExiting the sub-menu...\n')
                     break
                 else:
                     print('\nInvalid user input. Please try again!\n')
-        
+            conn.commit()
         # restart the database
         elif user_input == 'E':
             user_input = str(input('''Are you sure? 
-            This will delete all current data. (Y/N)''')).upper()
+            This will delete all current data. (Y/N): ''')).upper()
 
             if user_input == 'Y':
                 # delete current table and create new table
@@ -211,7 +214,7 @@ def main():
                 print('\nInitialized new database for contacts.\n')
             else:
                 print('\nContinuing the program...\n')
-        
+            conn.commit()
         # quit
         elif user_input == 'Q':
             user_input = str(input('Are you sure? (Y/N)')).upper()
